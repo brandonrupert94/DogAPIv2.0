112104784ce2059dee7e2470a8a19ba50ecb577a
Brandon Rupert <brupert94k@gmail.com>
2020-12-10 00:53:52 -0600

    fixed function to call images to DOM

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 function displayResults(responseJson){
     console.log(responseJson.message);
+    for (let i=0; i < responseJson.message.length; i++){
+    $('.results-container').append( `<img src="${responseJson.message[i]}" class="results-img">`)}
 }
 function getImage() {
+
     let imgAmount = document.getElementById('amount').value;
     let url = "https://dog.ceo/api/breeds/image/random/" + imgAmount;
-    $('#results').show;
+    
     fetch(url)
         .then(response => response.json())
         .then(responseJson => displayResults(responseJson))
         .catch(error => alert('Something went wrong. Try again later'));
 
-    
-}
 
+}
 function handleSubmit() {
     $('form').submit(event => {
         event.preventDefault();
@@ -27,6 +29,7 @@ function handleSubmit() {
 function loadApp() {
     console.log('App is loaded and ready');
     handleSubmit();
+    
 }
 
 $(loadApp());
